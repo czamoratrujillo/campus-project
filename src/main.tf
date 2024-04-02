35e122d0cb17e08cfb62b1e7627e06f4f0eb7d48
Cristopher Zamora <158361385+czamoratrujillo@users.noreply.github.com>
2024-04-02 16:16:57 -0600

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,31 +1,25 @@
+# use ubuntu 20 AMI for EC2 instance
 data "aws_ami" "ubuntu" {
     most_recent = true
-
-    filter {
+filter {
         name   = "name"
         values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
     }
-
-    filter {
+filter {
         name   = "virtualization-type"
         values = ["hvm"]
     }
-    
-    owners = ["099720109477"] # Canonical
+owners = ["099720109477"] # Canonical
 }
-
-##AWS
+# provision to us-east-2 region
 provider "aws" {
   region  = "us-east-2"
 }
-
-##EC2
-resource "aws_instance" "demo-ec2" {
+resource "aws_instance" "demo_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = "app-ssh-key"
-
-  tags = {
+tags = {
     Name = var.ec2_name
   }
 }
