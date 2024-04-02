2bd00c48cfe49eca19d50af183139f308f88bb0b
Cristopher Zamora <158361385+czamoratrujillo@users.noreply.github.com>
2024-04-02 16:12:52 -0600

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,13 +1,31 @@
-##AWS
-provider "aws" {
-  region = "us-east-1"
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+    owners = ["099720109477"] # Canonical
 }
 
-##AWS EC2
-resource "aws_instance" "campus-project-ec2" {
-    ami = "" #us-east-1
-    instace_type = "t.micro"
-    tags = {
-        "Name" = "EC2Instance"
-    }
+##AWS
+provider "aws" {
+  region  = "us-east-2"
+}
+
+##EC2
+resource "aws_instance" "demo-ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "app-ssh-key"
+
+  tags = {
+    Name = var.ec2_name
+  }
 }
